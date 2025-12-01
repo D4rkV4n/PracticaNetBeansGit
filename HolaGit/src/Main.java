45b2460299857ecd1adb7e4b9a4a12ea4d779c72
David <mail@mail.com>
2025-12-01 08:29:30 +0100

    Primer commit de la práctica NetBeans Github

---
+++ HolaGit/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 6DAWM
+ * 
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola desde NetBeans y Git. Soy David Jaime del Pozo");
+    }
+}
